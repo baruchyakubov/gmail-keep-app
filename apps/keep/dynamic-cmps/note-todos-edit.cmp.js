f73ca53aa92a6db65b5c6e5871de44b7ca0f4137
baruch yakubov <baruchyaku@gmail.com>
2023-01-25 12:22:08 +0200

    added edit features to keep app

--- apps/keep/dynamic-cmps/note-todos-edit.cmp.js
+++ apps/keep/dynamic-cmps/note-todos-edit.cmp.js
@@ -3,7 +3,7 @@ export default {
     template: `
         <section class="note-todos-edit" v-if="note">
             <input @input="$emit('setForm' , form)" type="text" v-model="form.label" />
-            <div v-for="(todo,idx) in form.todos" :key="todo.doneAt" >
+            <div v-for="(todo,idx) in form.todos"  >
                 <input @input="$emit('setForm' , form)" type="text" v-model="todo.txt" />
                 <button @click="removeTodo(idx)">X</button>
             </div>
@@ -12,18 +12,20 @@ export default {
     `,
     data() {
         return {
-            form: { label: { ...this.note.info }.label, todos: [...this.note.info.todos] }
+            form: {...this.note.info}
         }
     },
     methods: {
         addTodos() {
             this.form.todos.push({ txt: "", status: false })
+            this.$emit('setForm' , this.form)
         },
         removeTodo(todoIdx) {
             const idx = this.form.todos.findIndex((todo, idx) => {
                 return idx === todoIdx
             })
             this.form.todos.splice(idx, 1)
+            this.$emit('setForm' , this.form)
         }
     }
 }
